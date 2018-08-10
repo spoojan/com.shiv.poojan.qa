79acf85519f59a4993fd55ed91f836ce9c7b920f
Shiv Poojan <shivpjn87@gmail.com>
2018-08-10 16:52:48 +0530

    added practice problems

---
+++ com.shiv.poojan.qa/src/main/java/practice/SolutionParseDay.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.*;
+
+public class SolutionParseDay {
+
+	public static String getDay(String day, String month, String year) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Integer.parseInt(year), Integer.parseInt(month) - 1, Integer.parseInt(day));
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		String Day[] = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+		return (Day[dayOfWeek - 1]);
+
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String month = in.next();
+		String day = in.next();
+		String year = in.next();
+
+		System.out.println(getDay(day, month, year));
+	}
+}
